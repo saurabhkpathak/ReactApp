fe64be5f2f51a704b4f9aad75dcc785352bf7925
Saaurabh Pathak <saurabh.pathak@optimusinfo.com>
2015-04-26 19:52:35 +0530

    creating the main app file

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/** @jsx React.DOM */
+(function () {
+    'use strict';
+    var Quiz = React.createClass({
+        render: function() {
+            return <div>test</div>;
+        }
+    });
+    React.render(<Quiz data={"foo"} />, document.getElementById('app'));
+})();
